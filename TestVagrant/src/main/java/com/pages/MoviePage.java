3095c87b27b5db56d7f88d1a33af8e04a6cf635d
Bhanu Pratap <38413989+bhanupratap7161@users.noreply.github.com>
2022-09-08 15:52:34 +0530

    Update MoviePage.java

--- TestVagrant/src/main/java/com/pages/MoviePage.java
+++ TestVagrant/src/main/java/com/pages/MoviePage.java
@@ -8,12 +8,17 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import com.base.TestBase;
 import com.util.TestUtil;
 
 public class MoviePage extends TestBase 
 {
+	static String ExpectedReleasedate = "17 December 2021";
+	static String Expectedcountryorigin = "India";
+	
 
 public MoviePage() throws IOException 
 	{
@@ -21,7 +26,6 @@ public MoviePage() throws IOException
 	}
 	public static void ValidateDate() throws IOException
 	{
-		
 		TestUtil.geturl("wiki_url");
 		WebElement wifisearchinput = driver.findElement(By.xpath("//input[@id='searchInput']"));
 		wifisearchinput.sendKeys("Pushpa: The Rise");
@@ -30,23 +34,47 @@ public MoviePage() throws IOException
 		js.executeScript("window.scrollBy(0,600)");
 		WebElement wikireleasedate = driver.findElement(By.xpath("//*[@id='mw-content-text']/div[1]/table[1]/tbody/tr[12]/td/div/ul/li"));
 		String wikiRD = wikireleasedate.getText();
-		System.out.println("wifi release dadte : "+wikiRD);
-		WebElement wifiorigin= driver.findElement(By.xpath("//th[contains(text(),'Country')]//following-sibling::td"));
-		String wikior = wifiorigin.getText();
-		System.out.println("wifi origin : "+wikior);
+		Assert.assertEquals(ExpectedReleasedate,wikiRD);
+		
 		TestUtil.geturl("imbd_url");
 		WebElement imbdsearchinput= driver.findElement(By.xpath("//input[@placeholder='Search IMDb']"));
 		imbdsearchinput.sendKeys("Pushpa: The Rise - Part 1");
 		imbdsearchinput.sendKeys(Keys.ENTER);
 		driver.findElement(By.linkText("Pushpa: The Rise - Part 1")).click();
-		WebElement imdborigin= driver.findElement(By.xpath("//span[contains(text(),'Country of origin')]//ancestor::li/div/ul/li/a"));
-		String imdbor =imdborigin.getText();
-		System.out.println("imdb origin : "+imdbor);
 		WebElement imdbreleasedatebtn= driver.findElement(By.xpath("//a[contains(text(),'Release date')]"));
 		imdbreleasedatebtn.click();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		WebElement imbdmoviedate= driver.findElement(By.xpath("//a[contains(text(),'India')]//ancestor::td//following-sibling::td[1]"));
 		String imdbRD= imbdmoviedate.getText();
-		System.out.println("imdb release dadte : "+imdbRD);		
+		Assert.assertEquals(ExpectedReleasedate,imdbRD);
+		Assert.assertEquals(imdbRD,wikiRD);
+		System.out.println("Wikipedia release date = "+wikiRD +" is equal to imdb release date = "+imdbRD);
+	}
+	public static void Validateorigin() throws IOException 
+	{
+		TestUtil.geturl("wiki_url");
+		WebElement wifisearchinput = driver.findElement(By.xpath("//input[@id='searchInput']"));
+		wifisearchinput.sendKeys("Pushpa: The Rise");
+		wifisearchinput.sendKeys(Keys.ENTER);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,600)");
+		WebElement wifiorigin= driver.findElement(By.xpath("//th[contains(text(),'Country')]//following-sibling::td"));
+		String wikior = wifiorigin.getText();
+		System.out.println("wifi origin : "+wikior);
+		Assert.assertEquals(Expectedcountryorigin,wikior);
+		
+		TestUtil.geturl("imbd_url");
+		WebElement imbdsearchinput= driver.findElement(By.xpath("//input[@placeholder='Search IMDb']"));
+		imbdsearchinput.sendKeys("Pushpa: The Rise - Part 1");
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		imbdsearchinput.sendKeys(Keys.ENTER);
+		driver.findElement(By.linkText("Pushpa: The Rise - Part 1")).click();
+		WebElement imdborigin= driver.findElement(By.xpath("//span[contains(text(),'Country of origin')]//ancestor::li/div/ul/li/a"));
+		String imdbor =imdborigin.getText();
+		Assert.assertEquals(Expectedcountryorigin,imdbor);
+		Assert.assertEquals(wikior,imdbor);	
+		System.out.println("Wikipedia country origin ="+wikior +"is equal to country origin ="+imdbor);
+		
+		
 	}
 }
